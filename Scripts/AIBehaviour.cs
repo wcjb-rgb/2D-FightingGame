2c0a83093883aec0839f1bdb2f021cc250f646e0
wcjb-rgb <wcjbweerakoo@mun.ca>
2025-04-02 20:26:04 -0230

    Update AIBehaviour.cs

--- Scripts/AIBehaviour.cs
+++ Scripts/AIBehaviour.cs
@@ -1,1 +1,52 @@
+using Godot;
+using System;
 
+public partial class AIBehaviour : Node
+{
+    private Player player;
+    private Player AIplayer;
+    private float AISpeed = 380f;
+    private float range = 50f;
+    private bool attacking = false;
+
+    public override void _Ready()
+    {
+        AIplayer = (Player)GetParent();
+        player = GetNode<Player>("Player");
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        HandleMovement(delta);
+        HandleAttack();
+    }
+
+    private void HandleMovement(double delta)
+    {
+        float direction = AIplayer.Position.X - player.Position.X;
+        if (direction > 0)
+        {
+            Velocity = new Vector2(AISpeed,Velocity.Y);
+            _anim.Play("walk_forward");
+        }
+        else if (direction < 0)
+        {
+            Velocity = new Vector2(-AISpeed, Velocity.Y);
+            _anim.Play("walk_backward");
+        }
+        else
+        {
+            Velocity = Vecotr2.Zero;
+            PlayIdle();
+        }
+    }
+
+    private void HandleAttack()
+    {
+        if (AIplayer.Position.DistanceTo(player.Position) <range && !attacking)
+        {
+            attacking = true;
+            AIplayer.PerformAttack("LP");
+        } 
+    }
+}
